b074174630afed248363e08c0c99549f471a42f8
Sean Chalmers <seandchalmers@yahoo.ca>
2016-06-07 14:58:20 -0400

    Ooops

---
+++ main/src/environment/video.py
@@ -0,0 +1,25 @@
+"""
+Model the audio physical environment and translate to and from the
+Virtual Universe Machine
+
+"""
+
+import asyncio
+
+import numpy as np
+
+
+class Video:
+    """
+    A class implementing buffered video I/O.
+    """
+
+    @staticmethod
+    def createCapture(init_conds):
+        pass
+
+    pass
+
+
+class VideoCapture(Video):
+    pass
